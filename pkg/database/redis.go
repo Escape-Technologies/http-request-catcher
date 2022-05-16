6818fc481da71d9f052f08620a50ceb43fad3314
Swan Beaujard <dev@c3b5aw.dev>
2022-05-17 00:16:17 +0200

    chore: update database pattern

---
+++ pkg/database/redis.go
@@ -0,0 +1,73 @@
+package database
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/Escape-Technologies/http-request-catcher/api"
+	"github.com/Escape-Technologies/http-request-catcher/internal"
+	"github.com/go-redis/redis"
+	"github.com/nitishm/go-rejson"
+)
+
+func redisJsonSetup(client *redis.Client) {
+	rh := rejson.NewReJSONHandler()
+
+	rh.SetGoRedisClient(client)
+
+	DbHandler = rh
+}
+
+func redisConnect(host string, port string, password string, db int) *redis.Client {
+	fmt.Println("Connecting to redis server: " + host + ":" + port)
+	fmt.Printf("Connecting to redis database: %d\n", db)
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     host + ":" + port,
+		Password: password,
+		DB:       db,
+	})
+
+	if _, err := rdb.Ping().Result(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to redis server: " + host + ":" + port)
+
+	redisJsonSetup(rdb)
+
+	return rdb
+}
+
+func storeRequestRedis(r *api.BucketEntry) {
+
+	packedEntry, _ := json.Marshal(r)
+
+	_ = Db.HSet(r.BucketId, r.Id, packedEntry)
+	_ = Db.Expire(r.BucketId, internal.REQUEST_EXPIRATION_TIME)
+}
+
+func getBucketRedis(id string) api.Bucket {
+	data := Db.HGetAll(id)
+
+	entries := make(api.Bucket, 0)
+	for _, entry := range data.Val() {
+		jsonEntry := api.BucketEntry{}
+
+		_ = json.Unmarshal([]byte(entry), &jsonEntry)
+		entries = append(entries, jsonEntry)
+	}
+
+	return entries
+}
+
+func deleteBucketRedis(id string) int {
+	count := 0
+	fields := Db.HGetAll(id)
+	for field := range fields.Val() {
+		_ = Db.HDel(id, field)
+		count += 1
+	}
+
+	return count
+}
--- pkg/database/storage.go
+++ pkg/database/storage.go
@@ -1,19 +1,8 @@
 package database
 
 import (
-	"encoding/json"
-
 	"github.com/Escape-Technologies/http-request-catcher/api"
-	"github.com/Escape-Technologies/http-request-catcher/internal"
 )
-
-func storeRequestRedis(r *api.BucketEntry) {
-
-	packedEntry, _ := json.Marshal(r)
-
-	_ = Db.HSet(r.BucketId, r.Id, packedEntry)
-	_ = Db.Expire(r.BucketId, internal.REQUEST_EXPIRATION_TIME)
-}
 
 func StoreRequest(r *api.BucketEntry) {
 	switch Provider {
@@ -22,20 +11,6 @@ func StoreRequest(r *api.BucketEntry) {
 	default:
 		panic("Unknown database type")
 	}
-}
-
-func getBucketRedis(id string) api.Bucket {
-	data := Db.HGetAll(id)
-
-	entries := make(api.Bucket, 0)
-	for _, entry := range data.Val() {
-		jsonEntry := api.BucketEntry{}
-
-		_ = json.Unmarshal([]byte(entry), &jsonEntry)
-		entries = append(entries, jsonEntry)
-	}
-
-	return entries
 }
 
 func GetBucket(id string) api.Bucket {
@@ -50,15 +25,7 @@ func GetBucket(id string) api.Bucket {
 func DeleteBucket(id string) int {
 	switch Provider {
 	case api.REDIS:
-
-		count := 0
-		fields := Db.HGetAll(id)
-		for field := range fields.Val() {
-			_ = Db.HDel(id, field)
-			count += 1
-		}
-
-		return count
+		return deleteBucketRedis(id)
 	default:
 		panic("Unknown database type")
 	}
