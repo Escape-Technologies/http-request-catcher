725e9467f6db8ce18936b9f6b1fa06130daef649
swan_escape <swan@escape.tech>
2024-07-02 14:08:36 +0200

    feat: any slash support

--- pkg/http/router.go
+++ pkg/http/router.go
@@ -17,7 +17,7 @@ func SetupRouter(r *gin.Engine) {
 	r.GET("/:bucket_id", methods.FetchBucket)
 	r.DELETE("/:bucket_id", methods.RemoveBucket)
 
-	r.Any("/:bucket_id/:request_id", methods.CreateBucketEntry)
+	r.Any("/:bucket_id/*request_id", methods.CreateBucketEntry)
 }
 
 func StartRouter(r *gin.Engine, port int) {
